55f3520d39b7e95df9d9b038d9f7898ceb71f12d
smithb <smithb@wlwv.k12.or.us>
2022-10-19 08:12:05 -0700

    initial commit

---
+++ MathDemo.java
@@ -0,0 +1,20 @@
+public class MathDemo{
+    public static void main(String[] args){
+        
+        //absolute value
+        
+        
+        //square root something
+        
+        
+        //use exponents
+        
+        
+        //find the sin, cos, and tan of an angle
+        
+        
+        //area of a circle
+        
+        
+    }
+}
---
+++ RectangleTester.java
@@ -0,0 +1,19 @@
+public class RectangleTester
+{
+    public static void main(String[] args)
+    {
+        // Can call static methods
+        // WITHOUT creating an object
+        // Classname.method();
+        
+        
+        // Cannot call non-static methods without
+        // an object
+        
+        // WRONG
+        //int area = Rectangle.area();
+        
+        // RIGHT
+        
+    }
+}
