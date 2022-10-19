bc3e10840aaed78e42c3b58280f3be83b28f9ec6
smithb <smithb@wlwv.k12.or.us>
2022-10-19 08:21:48 -0700

    Update MathDemo.java
    added optional import statement

--- MathDemo.java
+++ MathDemo.java
@@ -1,7 +1,10 @@
+//import static java.lang.Math.*;
+
 public class MathDemo{
     public static void main(String[] args){
         
         //absolute value
+        
         
         
         //square root something
